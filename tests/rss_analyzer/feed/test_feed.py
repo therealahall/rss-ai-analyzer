af8faca37661a913d9d9c9f99f658c6c33b2413d
Aaron Hall <ahall@aaronhall.us>
2024-11-01 14:50:27 -0700

    test: breaking tests into valid vs invalid with more scope

--- tests/rss_analyzer/feed/test_feed.py
+++ tests/rss_analyzer/feed/test_feed.py
@@ -5,10 +5,18 @@ from rss_analyzer.exceptions import InvalidUrlException
 
 def test_valid_url() -> None:
     valid_url = "https://www.example.org"
-    invalid_url = "invalid url"
-
-    with pytest.raises(InvalidUrlException):
-        Feed(invalid_url)
-
     rss_feed = Feed(valid_url)
     assert rss_feed.url == valid_url
+
+
+def test_invalid_url() -> None:
+    invalid_urls = [
+        "invalid url",
+        "http://",
+        "definitely-not-a-url",
+        "",
+    ]
+
+    for invalid_url in invalid_urls:
+        with pytest.raises(InvalidUrlException):
+            Feed(invalid_url)
