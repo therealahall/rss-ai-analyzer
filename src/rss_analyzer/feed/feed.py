b7e432f0d3c0a20449219723f4da57077098bdb9
Aaron Hall <ahall@aaronhall.us>
2024-11-01 15:00:37 -0700

    docs: adding class and method doc strings

--- src/rss_analyzer/feed/feed.py
+++ src/rss_analyzer/feed/feed.py
@@ -1,12 +1,16 @@
-import validators
+"""Import the URL validation to ensure we have a valid URl before processing anything"""
+
+from validators import url as validateUrl
 from rss_analyzer.exceptions import InvalidUrlException
 
 
 class Feed:
+    """Our custom feed class to manage content coming from RSS feeds"""
+
     url: str
 
     def __init__(self, url: str) -> None:
-        if not validators.url(url):
+        if not validateUrl(url):
             raise InvalidUrlException(f"Invalid URL {url}")
 
         self.url = url
